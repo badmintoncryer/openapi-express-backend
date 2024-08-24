8bb87b9507dd567d990791156cb1a5099980959e
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2024-08-24 11:45:50 +0900

    udpate

--- src/index.ts
+++ src/index.ts
@@ -9,27 +9,30 @@ const __dirname = path.dirname(__filename);
 const apiSpec = path.join(__dirname, 'openapi.yaml');
 
 const app: express.Express = express()
+// configure the api validator
 app.use(
   OpenApiValidator.middleware({
     apiSpec,
     validateRequests: true,
     validateResponses: true,
     validateApiSpec: true,
+    operationHandlers: path.join(__dirname)
   }),
 );
-// app.use((err, req, res, next) => {
-//   // format error
-//   res.status(err.status || 500).json({
-//     message: err.message,
-//     errors: err.errors,
-//   });
-// });
+// error handler
+app.use((err: any, req: any, res: any, next: any) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors,
+  });
+});
+
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: false }));
-app.use('/spec', express.static(apiSpec));
 
-app.get("/", (req:express.Request, res:express.Response)=>{
+// define api
+app.get("/a", (req:express.Request, res:express.Response)=>{
     res.send({
       message: 'test'
     });
