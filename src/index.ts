96bb0bf39482b53d4a784820334ff4638e0b9905
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2024-08-24 15:35:04 +0900

    chore biome

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
-import express from 'express'
-import * as OpenApiValidator from 'express-openapi-validator'
-import path from 'path'
-import { fileURLToPath } from 'url';
+import express, {
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
+import * as OpenApiValidator from "express-openapi-validator";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const apiSpec = path.join(__dirname, 'openapi.yaml');
+const apiSpec = path.join(__dirname, "openapi.yaml");
 
-const app: express.Express = express()
+const app: express.Express = express();
 // configure the api validator
 app.use(
   OpenApiValidator.middleware({
@@ -16,27 +20,36 @@ app.use(
     validateRequests: true,
     validateResponses: true,
     validateApiSpec: true,
-    operationHandlers: path.join(__dirname)
+    operationHandlers: path.join(__dirname),
   }),
 );
+
 // error handler
-app.use((err: any, req: any, res: any, next: any) => {
-  res.status(err.status || 500).json({
-    message: err.message,
-    errors: err.errors,
-  });
-});
+app.use(
+  (
+    // biome-ignore lint/suspicious/noExplicitAny: unavoidable any
+    err: Error & { status: number; errors: any },
+    _req: Request,
+    res: Response,
+    _next: NextFunction,
+  ) => {
+    res.status(err.status || 500).json({
+      message: err.message,
+      errors: err.errors,
+    });
+  },
+);
 
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: false }));
 
 // define api
-app.get("/a", (req:express.Request, res:express.Response)=>{
-    res.send({
-      message: 'test'
-    });
-})
-app.listen(3000,()=>{
-    console.log('Start express server listening on port 3000');
-})
+app.get("/a", (req: express.Request, res: express.Response) => {
+  res.send({
+    message: "test",
+  });
+});
+app.listen(3000, () => {
+  console.log("Start express server listening on port 3000");
+});
